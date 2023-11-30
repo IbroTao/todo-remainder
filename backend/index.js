944a30c570b90b1d5a2c46e99767d28c73bf01bb
IbroTao <ibrahimtaofeek005@gmail.com>
2023-11-30 02:50:42 +0100

    Users APIs working perfectly

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ require("dotenv").config();
 
 const app = express();
 
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 app.use("/api/auth", authRouter);
 
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 
 const signupUser = async (req, res) => {
   try {
-    const { username, email, password } = req.body;
     console.log(req.body);
+    const { username, email, password } = req.body;
     const findUser = await User.findOne({ email });
     if (findUser) res.status(400).json("Email already used");
     await User.create({
@@ -29,25 +29,33 @@ const loginUser = async (req, res) => {
     const comparePassword = await compareSync(password, user.password);
     if (!comparePassword) res.status(400).json("Incorrect password");
 
-    const accessToken = jwt.sign(
+    const accessToken = await jwt.sign(
       {
-        id: user._id,
-        isAdmin: req.user.isAdmin,
-        restrictUser: req.user.restrictUser,
+        sub: user._id,
+        isAdmin: user.isAdmin,
+        restrictUser: user.restrictUser,
       },
-      {
-        Secret: process.env.SECRET,
-      },
+      process.env.SECRET,
       {
         expiresIn: "3d",
       }
     );
 
-    const allowUser = await User.findById(user._id).select["-password"];
+    const allowUser = await User.findById(user._id).select(["-password"]);
     res.status(200).json({
       user: allowUser,
       token: accessToken,
     });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByIdAndUpdate(id);
+    res.status(200).json("User deleted");
   } catch (err) {
     res.status(500).json(err);
   }
@@ -112,6 +120,12 @@ const updateUserEmail = async (req, res) => {
   try {
     let user = await User.findById(userData.id);
     if (user) {
+      const comparePassword = compare(userData.password, user.password);
+      if (comparePassword) {
+        user.email = userData.email;
+        user = await user.save();
+        res.status(200).json("User email updated");
+      } else res.status(400).json("Wrong password");
     } else {
       res.status(404).json("User not found");
     }
@@ -123,7 +137,9 @@ const updateUserEmail = async (req, res) => {
 module.exports = {
   signupUser,
   loginUser,
+  deleteUser,
   getUser,
   getAllUsers,
   updateUserPassword,
+  updateUserEmail,
 };
