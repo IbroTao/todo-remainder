4fed85f9443bc19e72e17eb00c04c5cf9ae386d3
BravoTechArena <klordbravo@gmail.com>
2023-11-30 08:20:50 +0100

    made minor changes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
+const todoRouter = require("./routes/todos");
+
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use("/todos", todoRouter);
 
 app.listen("3001", () => {
   console.log("server running at port 3001");
